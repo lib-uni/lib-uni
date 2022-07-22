117050119ffb0418f49444a396e5250da7fda09b
Daniil Shvalov <shvalovich@gmail.com>
2022-07-22 20:04:47 +0300

    chore: format data.h

--- include/uni/data.h
+++ include/uni/data.h
@@ -8,21 +8,21 @@
 #include <uni/config.h>
 
 #ifndef UNI_ALGO_DISABLE_CASE
-#include <uni/impl/case_data_extern.h>
-#include <uni/impl/case_data.h>
+    #include <uni/impl/case_data_extern.h>
+    #include <uni/impl/case_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_NORM
-#include <uni/impl/norm_data_extern.h>
-#include <uni/impl/norm_data.h>
+    #include <uni/impl/norm_data_extern.h>
+    #include <uni/impl/norm_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_BREAK_GRAPHEME
-#include <uni/impl/break_grapheme_data_extern.h>
-#include <uni/impl/break_grapheme_data.h>
+    #include <uni/impl/break_grapheme_data_extern.h>
+    #include <uni/impl/break_grapheme_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_BREAK_WORD
-#include <uni/impl/break_word_data_extern.h>
-#include <uni/impl/break_word_data.h>
+    #include <uni/impl/break_word_data_extern.h>
+    #include <uni/impl/break_word_data.h>
 #endif
