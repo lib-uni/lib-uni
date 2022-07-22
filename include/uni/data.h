fb5f109a1d72a92160fbfed25d4e09c0921d63c4
Daniil Shvalov <shvalovich@gmail.com>
2022-07-22 19:59:58 +0300

    fix: revert data.h

--- include/uni/data.h
+++ include/uni/data.h
@@ -2,27 +2,27 @@
  * License: Public Domain or MIT - choose whatever you want.
  * See LICENSE.md */
 
-#include <uni/config.h>
-
 #include <cstddef>
 #include <type_traits>
 
+#include <uni/config.h>
+
 #ifndef UNI_ALGO_DISABLE_CASE
-    #include <uni/impl/case_data.h>
-    #include <uni/impl/case_data_extern.h>
+#include <uni/impl/case_data_extern.h>
+#include <uni/impl/case_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_NORM
-    #include <uni/impl/norm_data.h>
-    #include <uni/impl/norm_data_extern.h>
+#include <uni/impl/norm_data_extern.h>
+#include <uni/impl/norm_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_BREAK_GRAPHEME
-    #include <uni/impl/break_grapheme_data.h>
-    #include <uni/impl/break_grapheme_data_extern.h>
+#include <uni/impl/break_grapheme_data_extern.h>
+#include <uni/impl/break_grapheme_data.h>
 #endif
 
 #ifndef UNI_ALGO_DISABLE_BREAK_WORD
-    #include <uni/impl/break_word_data.h>
-    #include <uni/impl/break_word_data_extern.h>
+#include <uni/impl/break_word_data_extern.h>
+#include <uni/impl/break_word_data.h>
 #endif
